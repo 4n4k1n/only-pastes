e60b623f3641ea2b8338b503ed5cb885c79f69e2
Anakin Pregitzer <anakin@pregitzer.de>
2025-12-18 13:14:03 +0000

    add: swagger route and main docs

--- main.go
+++ main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"log"
 	"only-pastes/database"
+	_ "only-pastes/docs"
 	"only-pastes/handlers"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title Only Pastes API
+// @version 1.0
+// @description A simple pastebin service API
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.email support@onlypastes.com
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:8080
+// @BasePath /api
 
 func main() {
 	db, err := database.ConnectDatabase()
@@ -22,9 +39,11 @@ func main() {
 
 	router := gin.Default()
 
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	router.POST("/api/paste", handlers.CreatePaste)
 	router.GET("/api/paste/:slug", handlers.GetPaste)
-
+	
 	router.Static("/static", "./static")
 	router.StaticFile("/", "./static/index.html")
 
